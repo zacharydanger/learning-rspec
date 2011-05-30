50fdd2c20433386cf36b214265a0d3820739b38d
Zachary Danger Campbell <zacharydangercampbell@gmail.com>
2011-05-30 09:18:34 -0500

    Refactor Game class a bit

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -11,19 +11,19 @@ module Codebreaker
     end
 
     def guess(guess)
-      mark = ''
-      (0..3).each do |index|
-        if exact_match?(guess, index)
-          mark << '+'
-        end
-      end
+      @output.puts '+'*exact_match_count(guess) + '-'*number_match_count(guess)
+    end
 
-      (0..3).each do |index|
-        if number_match?(guess, index)
-          mark << '-'
-        end
+    def exact_match_count(guess)
+      (0..3).inject(0) do |count, index|
+        count + (exact_match?(guess, index) ? 1 : 0)
       end
-      @output.puts mark
+    end
+
+    def number_match_count(guess)
+      (0..3).inject(0) do |count, index|
+        count + (number_match?(guess, index) ? 1 : 0)
+      end
     end
 
     def exact_match?(guess, index)
