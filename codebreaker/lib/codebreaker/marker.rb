23a8667531158f9b89f55cc22e5561c6a626cde6
Zachary Danger Campbell <zacharydangercampbell@gmail.com>
2011-05-30 10:28:01 -0500

    Final refactoring

--- codebreaker/lib/codebreaker/marker.rb
+++ codebreaker/lib/codebreaker/marker.rb
@@ -15,15 +15,14 @@ module Codebreaker
     end
 
     def total_match_count
-      count = 0
       secret = @secret.split('')
-      @guess.split('').map do |n|
-        if secret.include?(n)
-          secret.delete_at(@secret.index(n))
-          count += 1
-        end
+      @guess.split('').inject(0) do |count, n|
+        count + (delete_first(secret, n) ? 1 : 0)
       end
-      count
+    end
+
+    def delete_first(code, n)
+      code.delete_at(code.index(n)) if code.index(n)
     end
 
     def exact_match?(index)
