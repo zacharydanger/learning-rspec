61eceaf8e9387dfd79a3ba3d860dd56a2781ff4e
Zachary Danger Campbell <zacharydangercampbell@gmail.com>
2011-06-05 10:04:46 -0500

    Generate new "showtime" rails project

---
+++ showtime/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Showtime::Application.config.secret_token = '4aaab790e337256ae6eff0923df2766b8eb7055123cd96c658047e1a378448b1c23e2b3cd235471e5470b0b179bce048688df79d206c8215c43b27fbbf08ba17'
---
+++ showtime/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Showtime::Application.config.session_store :cookie_store, :key => '_showtime_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Showtime::Application.config.session_store :active_record_store
